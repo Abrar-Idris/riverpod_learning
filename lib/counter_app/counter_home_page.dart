3c1c26e0f6d7e14ca30be124aa4a608f9a328393
abrar-idrisi <abrarcoding3334@gmail.com>
2026-06-17 12:36:39 +0530

    day 5 of riverpod

--- lib/counter_app/counter_home_page.dart
+++ lib/counter_app/counter_home_page.dart
@@ -7,12 +7,28 @@ final counter = StateProvider<int>((ref){
   return 0;
 });
 
-class CounterHomePage extends ConsumerWidget {
+final  switchProvider = StateProvider<bool>((ref){
+  return false;
+});
+
+
+
+class CounterHomePage extends ConsumerStatefulWidget {
   const CounterHomePage({super.key});
 
   @override
-  Widget build(BuildContext context,WidgetRef ref) {
-    return Scaffold(
+  ConsumerState<CounterHomePage> createState() => _CounterHomePageState();
+}
+
+class _CounterHomePageState extends ConsumerState<CounterHomePage> {
+  @override
+  void initState(){
+   super.initState();
+   ref.read(switchProvider.notifier).state = false;
+  }
+  @override
+  Widget build(BuildContext context) {
+     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.indigoAccent,
         title: Text(
@@ -23,29 +39,41 @@ class CounterHomePage extends ConsumerWidget {
         children: [
           Center(
             child: Consumer(
-              builder: (context,ref,child) {
-                final count = ref.watch(counter);
-                return Text(count.toString(),
-                  style: TextStyle(
-                    fontSize: 25,
-                  ),
-                );
-              }
+                builder: (context,ref,child) {
+                  final count = ref.watch(counter);
+                  return Text(count.toString(),
+                    style: TextStyle(
+                      fontSize: 25,
+                    ),
+                  );
+                }
             ),
           ),
-          
+
+          Center(
+            child: Consumer(
+                builder: (context,ref,child) {
+                  final count = ref.watch(switchProvider);
+                  print("build 3 ");
+                  return Switch(value: count, onChanged:(value){
+                    ref.read(switchProvider.notifier).state = value;
+                  });
+                }
+            ),
+          ),
+
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               ElevatedButton(onPressed: (){
                 ref.read(counter.notifier).state++;
-              }, 
+              },
                   child: Icon(Icons.add)
               ),
               ElevatedButton(onPressed: (){
                 ref.read(counter.notifier).state--;
               },
-                  child: Icon(Icons.minimize),
+                child: Icon(Icons.minimize),
               ),
             ],
           )
@@ -54,3 +82,64 @@ class CounterHomePage extends ConsumerWidget {
     );
   }
 }
+
+
+// class CounterHomePage extends ConsumerWidget {
+//   const CounterHomePage({super.key});
+//
+//   @override
+//   Widget build(BuildContext context,WidgetRef ref) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Colors.indigoAccent,
+//         title: Text(
+//           "Counter App",
+//         ),
+//       ),
+//       body: Column(
+//         children: [
+//           Center(
+//             child: Consumer(
+//               builder: (context,ref,child) {
+//                 final count = ref.watch(counter);
+//                 return Text(count.toString(),
+//                   style: TextStyle(
+//                     fontSize: 25,
+//                   ),
+//                 );
+//               }
+//             ),
+//           ),
+//
+//           Center(
+//             child: Consumer(
+//                 builder: (context,ref,child) {
+//                   final count = ref.watch(switchProvider);
+//                   print("build 3 ");
+//                   return Switch(value: count, onChanged:(value){
+//                     ref.read(switchProvider.notifier).state = value;
+//                   });
+//                 }
+//             ),
+//           ),
+//
+//           Row(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               ElevatedButton(onPressed: (){
+//                 ref.read(counter.notifier).state++;
+//               },
+//                   child: Icon(Icons.add)
+//               ),
+//               ElevatedButton(onPressed: (){
+//                 ref.read(counter.notifier).state--;
+//               },
+//                   child: Icon(Icons.minimize),
+//               ),
+//             ],
+//           )
+//         ],
+//       ),
+//     );
+//   }
+// }
